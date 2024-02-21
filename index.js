8ab86f8b48757b5c0312caf44362cb6e37603089
Milad <milad@3sidedcube.com>
2024-02-21 14:01:40 +0000

    updated the script to a cli

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-const Sync = require('./src/sync')
-const StoryblokClient = require('storyblok-js-client')
-const sourceClient = new StoryblokClient({
-  oauthToken: 'SOURCE_OAUTH_TOKEN'
-})
-const targetClient = new StoryblokClient({
-  oauthToken: 'TARGET_OAUTH_TOKEN'
-}, 'https://8xhrfleis8.execute-api.cn-north-1.amazonaws.com.cn/live/v1')
+const yargs = require("yargs");
+const Sync = require("./src/sync");
+const StoryblokClient = require("storyblok-js-client");
 
-Sync(['Assets', 'Components', 'Folders', 'Stories'], {
-  sourceSpaceId: SOURCE_SPACE_ID,
-  targetSpaceId: TARGET_SPACE_ID,
+const argv = yargs
+  .option("sourceOauthToken", {
+    description: "Source oauthToken",
+    type: "string",
+    demandOption: true,
+  })
+  .option("targetOauthToken", {
+    description: "Target oauthToken",
+    type: "string",
+    demandOption: false,
+  })
+  .option("sourceSpaceId", {
+    description: "Source Space ID",
+    type: "string",
+    demandOption: true,
+  })
+  .option("targetSpaceId", {
+    description: "Target Space ID",
+    type: "string",
+    demandOption: true,
+  })
+  .option("types", {
+    description:
+      "Types to sync including, 'assets', 'components', 'folders', 'stories' use space separated for multiple types",
+    type: "array",
+    demandOption: true,
+  }).argv;
+
+const {
+  sourceOauthToken,
+  targetOauthToken,
+  sourceSpaceId,
+  targetSpaceId,
+  types,
+} = argv;
+
+const sourceClient = new StoryblokClient({
+  oauthToken: sourceOauthToken,
+});
+const targetClient = new StoryblokClient({
+  oauthToken: targetOauthToken || sourceOauthToken,
+});
+
+Sync(types, {
+  sourceSpaceId: sourceSpaceId,
+  targetSpaceId: targetSpaceId,
   sourceClient: sourceClient,
-  targetClient: targetClient
-})
+  targetClient: targetClient,
+});
--- src/sync.js
+++ src/sync.js
@@ -1,49 +1,58 @@
-const chalk = require('chalk')
-const StoryblokClient = require('storyblok-js-client')
-const pSeries = require('p-series')
-const fileUploader = require('./fileUploader')
+const chalk = require("chalk");
+const StoryblokClient = require("storyblok-js-client");
+const pSeries = require("p-series");
+const fileUploader = require("./fileUploader");
+const { capitalizeFirstLetter } = require("./helper");
 
 const SyncSpaces = {
   targetComponents: [],
   sourceComponents: [],
   assetMapping: {},
 
-  init (options) {
-    console.log(chalk.green('✓') + ' Loading options')
-    this.sourceSpaceId = options.sourceSpaceId
-    this.targetSpaceId = options.targetSpaceId
-    this.sourceClient = options.sourceClient
-    this.targetClient = options.targetClient
+  init(options) {
+    console.log(chalk.green("✓") + " Loading options");
+    this.sourceSpaceId = options.sourceSpaceId;
+    this.targetSpaceId = options.targetSpaceId;
+    this.sourceClient = options.sourceClient;
+    this.targetClient = options.targetClient;
   },
 
   async syncAssets() {
-    var all = await this.sourceClient.getAll(`spaces/${this.sourceSpaceId}/assets`)
-    this.assetMapping = {}
+    var all = await this.sourceClient.getAll(
+      `spaces/${this.sourceSpaceId}/assets`
+    );
+    this.assetMapping = {};
 
     for (let i = 0; i < all.length; i++) {
-      let fullFilename = all[i].filename
-      let cleanFilename = fullFilename.replace('https://s3.amazonaws.com/', '//')
-      let fileparts = fullFilename.split('/')
-      let filename = fileparts[fileparts.length-1]
-      let folder = fileparts[fileparts.length-2]
-      console.log(chalk.green('✓') + ' Starting transfer of asset ' + filename)
+      let fullFilename = all[i].filename;
+      let cleanFilename = fullFilename.replace(
+        "https://s3.amazonaws.com/",
+        "//"
+      );
+      let fileparts = fullFilename.split("/");
+      let filename = fileparts[fileparts.length - 1];
+      let folder = fileparts[fileparts.length - 2];
+      console.log(chalk.green("✓") + " Starting transfer of asset " + filename);
 
       try {
-        let response = await this.targetClient.post('spaces/' + this.targetSpaceId + '/assets', {
-          filename: filename,
-          ext_id: folder + '/' + filename
-        })
-        this.assetMapping[cleanFilename] = response.data.public_url
+        let response = await this.targetClient.post(
+          "spaces/" + this.targetSpaceId + "/assets",
+          {
+            filename: filename,
+            ext_id: folder + "/" + filename,
+          }
+        );
+        this.assetMapping[cleanFilename] = response.data.public_url;
 
-        fileUploader(all[i].filename, response.data, function(file) {
-          console.log(chalk.green('✓') + ' File ' + file + ' uploaded')
-        })
+        fileUploader(all[i].filename, response.data, function (file) {
+          console.log(chalk.green("✓") + " File " + file + " uploaded");
+        });
       } catch (e) {
         if (e.response.status === 422) {
-          console.log('This asset already exists')
-          this.assetMapping[cleanFilename] = e.response.data.filename
+          console.log("This asset already exists");
+          this.assetMapping[cleanFilename] = e.response.data.filename;
         } else {
-          throw e
+          throw e;
         }
       }
     }
@@ -53,235 +62,309 @@ const SyncSpaces = {
     var traverse = (jtree) => {
       if (jtree.constructor === String) {
         for (var sourceImg in this.assetMapping) {
-          jtree = jtree.replace(sourceImg, this.assetMapping[sourceImg])
-          console.log(jtree)
+          jtree = jtree.replace(sourceImg, this.assetMapping[sourceImg]);
+          console.log(jtree);
         }
       } else if (jtree.constructor === Array) {
         for (var item = 0; item < jtree.length; item++) {
-          traverse(jtree[item])
+          traverse(jtree[item]);
         }
       } else if (jtree.constructor === Object) {
         for (var treeItem in jtree) {
-          traverse(jtree[treeItem])
+          traverse(jtree[treeItem]);
         }
       }
-    }
+    };
 
-    traverse(content)
-    return content
+    traverse(content);
+    return content;
   },
 
-  async syncRelinkAssets () {
-    var all = await this.targetClient.getAll(`spaces/${this.targetSpaceId}/stories`, {
-      story_only: 1
-    })
+  async syncRelinkAssets() {
+    var all = await this.targetClient.getAll(
+      `spaces/${this.targetSpaceId}/stories`,
+      {
+        story_only: 1,
+      }
+    );
 
     for (let i = 0; i < all.length; i++) {
-      var resp = await this.targetClient.get(`spaces/${this.targetSpaceId}/stories/${all[i].id}`)
-      var content = this.replaceAssets(resp.data.story.content)
+      var resp = await this.targetClient.get(
+        `spaces/${this.targetSpaceId}/stories/${all[i].id}`
+      );
+      var content = this.replaceAssets(resp.data.story.content);
 
-      await this.targetClient.put('spaces/' + this.targetSpaceId + '/stories/' + resp.data.story.id, {
-        story: {
-          content: resp.data.story.content
+      await this.targetClient.put(
+        "spaces/" + this.targetSpaceId + "/stories/" + resp.data.story.id,
+        {
+          story: {
+            content: resp.data.story.content,
+          },
         }
-      })
-      console.log(chalk.green('✓') + ' Updated ' + resp.data.story.full_slug)
+      );
+      console.log(chalk.green("✓") + " Updated " + resp.data.story.full_slug);
     }
   },
 
-  async syncStories () {
-    console.log(chalk.green('✓') + ' Syncing stories...')
-    var targetFolders = await this.targetClient.getAll(`spaces/${this.targetSpaceId}/stories`, {
-      folder_only: 1,
-      sort_by: 'slug:asc'
-    })
+  async syncStories() {
+    console.log(chalk.green("✓") + " Syncing stories...");
+    var targetFolders = await this.targetClient.getAll(
+      `spaces/${this.targetSpaceId}/stories`,
+      {
+        folder_only: 1,
+        sort_by: "slug:asc",
+      }
+    );
 
-    var folderMapping = {}
+    var folderMapping = {};
 
     for (let i = 0; i < targetFolders.length; i++) {
-      var folder = targetFolders[i]
-      folderMapping[folder.full_slug] = folder.id
+      var folder = targetFolders[i];
+      folderMapping[folder.full_slug] = folder.id;
     }
 
-    var all = await this.sourceClient.getAll(`spaces/${this.sourceSpaceId}/stories`, {
-      story_only: 1
-    })
+    var all = await this.sourceClient.getAll(
+      `spaces/${this.sourceSpaceId}/stories`,
+      {
+        story_only: 1,
+      }
+    );
 
     for (let i = 0; i < all.length; i++) {
-      console.log(chalk.green('✓') + ' Starting update ' + all[i].full_slug)
+      console.log(chalk.green("✓") + " Starting update " + all[i].full_slug);
 
-      var storyResult = await this.sourceClient.get('spaces/' + this.sourceSpaceId + '/stories/' + all[i].id)
-      var sourceStory = storyResult.data.story
-      var slugs = sourceStory.full_slug.split('/')
-      var folderId = 0
+      var storyResult = await this.sourceClient.get(
+        "spaces/" + this.sourceSpaceId + "/stories/" + all[i].id
+      );
+      var sourceStory = storyResult.data.story;
+      var slugs = sourceStory.full_slug.split("/");
+      var folderId = 0;
 
       if (slugs.length > 1) {
-        slugs.pop()
-        var folderSlug = slugs.join('/')
+        slugs.pop();
+        var folderSlug = slugs.join("/");
 
         if (folderMapping[folderSlug]) {
-          folderId = folderMapping[folderSlug]
+          folderId = folderMapping[folderSlug];
         } else {
-          console.error(chalk.red('X') + 'The folder does not exist ' + folderSlug)
-          continue
+          console.error(
+            chalk.red("X") + "The folder does not exist " + folderSlug
+          );
+          continue;
         }
       }
 
-      sourceStory.parent_id = folderId
+      sourceStory.parent_id = folderId;
 
       try {
-        var existingStory = await this.targetClient.get('spaces/' + this.targetSpaceId + '/stories', { with_slug: all[i].full_slug })
+        var existingStory = await this.targetClient.get(
+          "spaces/" + this.targetSpaceId + "/stories",
+          { with_slug: all[i].full_slug }
+        );
         var payload = {
           story: sourceStory,
-          force_update: '1'
-        }
+          force_update: "1",
+        };
         if (sourceStory.published) {
-          payload.publish = '1'
+          payload.publish = "1";
         }
 
         if (existingStory.data.stories.length === 1) {
-          await this.targetClient.put('spaces/' + this.targetSpaceId + '/stories/' + existingStory.data.stories[0].id, payload)
-          console.log(chalk.green('✓') + ' Updated ' + existingStory.data.stories[0].full_slug)
+          await this.targetClient.put(
+            "spaces/" +
+              this.targetSpaceId +
+              "/stories/" +
+              existingStory.data.stories[0].id,
+            payload
+          );
+          console.log(
+            chalk.green("✓") +
+              " Updated " +
+              existingStory.data.stories[0].full_slug
+          );
         } else {
-          await this.targetClient.post('spaces/' + this.targetSpaceId + '/stories', payload)
-          console.log(chalk.green('✓') + ' Created ' + sourceStory.full_slug)
+          await this.targetClient.post(
+            "spaces/" + this.targetSpaceId + "/stories",
+            payload
+          );
+          console.log(chalk.green("✓") + " Created " + sourceStory.full_slug);
         }
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     }
 
-    return Promise.resolve(all)
+    return Promise.resolve(all);
   },
 
-  async syncFolders () {
-    console.log(chalk.green('✓') + ' Syncing folders...')
-    const sourceFolders = await this.sourceClient.getAll(`spaces/${this.sourceSpaceId}/stories`, {
-      folder_only: 1,
-      sort_by: 'slug:asc'
-    })
-    const syncedFolders = {}
+  async syncFolders() {
+    console.log(chalk.green("✓") + " Syncing folders...");
+    const sourceFolders = await this.sourceClient.getAll(
+      `spaces/${this.sourceSpaceId}/stories`,
+      {
+        folder_only: 1,
+        sort_by: "slug:asc",
+      }
+    );
+    const syncedFolders = {};
 
     for (var i = 0; i < sourceFolders.length; i++) {
-      const folder = sourceFolders[i]
-      const folderId = folder.id
-      delete folder.id
-      delete folder.created_at
+      const folder = sourceFolders[i];
+      const folderId = folder.id;
+      delete folder.id;
+      delete folder.created_at;
 
       if (folder.parent_id) {
         // Parent child resolving
         if (!syncedFolders[folderId]) {
-          const folderSlug = folder.full_slug.split('/')
-          const parentFolderSlug = folderSlug.splice(0, folderSlug.length - 1).join('/')
+          const folderSlug = folder.full_slug.split("/");
+          const parentFolderSlug = folderSlug
+            .splice(0, folderSlug.length - 1)
+            .join("/");
 
-          const existingFolders = await this.targetClient.get(`spaces/${this.targetSpaceId}/stories`, {
-            with_slug: parentFolderSlug
-          })
+          const existingFolders = await this.targetClient.get(
+            `spaces/${this.targetSpaceId}/stories`,
+            {
+              with_slug: parentFolderSlug,
+            }
+          );
 
           if (existingFolders.data.stories.length) {
-            folder.parent_id = existingFolders.data.stories[0].id
+            folder.parent_id = existingFolders.data.stories[0].id;
           } else {
-            folder.parent_id = 0
+            folder.parent_id = 0;
           }
         } else {
-          folder.parent_id = syncedFolders[folderId]
+          folder.parent_id = syncedFolders[folderId];
         }
       }
 
       try {
-        const newFolder = await this.targetClient.post(`spaces/${this.targetSpaceId}/stories`, {
-          story: folder
-        })
+        const newFolder = await this.targetClient.post(
+          `spaces/${this.targetSpaceId}/stories`,
+          {
+            story: folder,
+          }
+        );
 
-        syncedFolders[folderId] = newFolder.data.story.id
-        console.log(`Folder ${newFolder.data.story.name} created`)
+        syncedFolders[folderId] = newFolder.data.story.id;
+        console.log(`Folder ${newFolder.data.story.name} created`);
       } catch (e) {
-        console.log(`Folder ${folder.name} not created. It may exists already. (${e.message})`)
+        console.log(
+          `Folder ${folder.name} not created. It may exists already. (${e.message})`
+        );
       }
     }
   },
 
-  async syncRoles () {
-    console.log(chalk.green('✓') + ' Syncing roles...')
-    const existingFolders = await this.targetClient.getAll(`spaces/${this.targetSpaceId}/stories`, {
-      folder_only: 1,
-      sort_by: 'slug:asc'
-    })
+  async syncRoles() {
+    console.log(chalk.green("✓") + " Syncing roles...");
+    const existingFolders = await this.targetClient.getAll(
+      `spaces/${this.targetSpaceId}/stories`,
+      {
+        folder_only: 1,
+        sort_by: "slug:asc",
+      }
+    );
 
-    const roles = await this.sourceClient.get(`spaces/${this.sourceSpaceId}/space_roles`)
-    const existingRoles = await this.targetClient.get(`spaces/${this.targetSpaceId}/space_roles`)
+    const roles = await this.sourceClient.get(
+      `spaces/${this.sourceSpaceId}/space_roles`
+    );
+    const existingRoles = await this.targetClient.get(
+      `spaces/${this.targetSpaceId}/space_roles`
+    );
 
     for (var i = 0; i < roles.data.space_roles.length; i++) {
-      const spaceRole = roles.data.space_roles[i]
-      delete spaceRole.id
-      delete spaceRole.created_at
+      const spaceRole = roles.data.space_roles[i];
+      delete spaceRole.id;
+      delete spaceRole.created_at;
 
-      spaceRole.allowed_paths = []
+      spaceRole.allowed_paths = [];
 
       spaceRole.resolved_allowed_paths.forEach((path) => {
         const folders = existingFolders.filter((story) => {
-          return story.full_slug + '/' === path
-        })
+          return story.full_slug + "/" === path;
+        });
 
         if (folders.length) {
-          spaceRole.allowed_paths.push(folders[0].id)
+          spaceRole.allowed_paths.push(folders[0].id);
         }
-      })
+      });
 
       const existingRole = existingRoles.data.space_roles.filter((role) => {
-        return role.role === spaceRole.role
-      })
+        return role.role === spaceRole.role;
+      });
       if (existingRole.length) {
-        await this.targetClient.put(`spaces/${this.targetSpaceId}/space_roles/${existingRole[0].id}`, {
-          space_role: spaceRole
-        })
+        await this.targetClient.put(
+          `spaces/${this.targetSpaceId}/space_roles/${existingRole[0].id}`,
+          {
+            space_role: spaceRole,
+          }
+        );
       } else {
-        await this.targetClient.post(`spaces/${this.targetSpaceId}/space_roles`, {
-          space_role: spaceRole
-        })
+        await this.targetClient.post(
+          `spaces/${this.targetSpaceId}/space_roles`,
+          {
+            space_role: spaceRole,
+          }
+        );
       }
-      console.log(chalk.green('✓') + ` Role ${spaceRole.role} synced`)
+      console.log(chalk.green("✓") + ` Role ${spaceRole.role} synced`);
     }
   },
 
-  async syncComponents () {
-    console.log(chalk.green('✓') + ' Syncing components...')
-    this.targetComponents = await this.targetClient.get(`spaces/${this.targetSpaceId}/components`)
-    this.sourceComponents = await this.sourceClient.get(`spaces/${this.sourceSpaceId}/components`)
+  async syncComponents() {
+    console.log(chalk.green("✓") + " Syncing components...");
+    this.targetComponents = await this.targetClient.get(
+      `spaces/${this.targetSpaceId}/components`
+    );
+    this.sourceComponents = await this.sourceClient.get(
+      `spaces/${this.sourceSpaceId}/components`
+    );
 
     for (var i = 0; i < this.sourceComponents.data.components.length; i++) {
-      const component = this.sourceComponents.data.components[i]
+      const component = this.sourceComponents.data.components[i];
 
-      delete component.id
-      delete component.created_at
+      delete component.id;
+      delete component.created_at;
 
       // Create new component on target space
       try {
-        await this.targetClient.post(`spaces/${this.targetSpaceId}/components`, {
-          component: component
-        })
-        console.log(chalk.green('✓') + ` Component ${component.name} synced`)
+        await this.targetClient.post(
+          `spaces/${this.targetSpaceId}/components`,
+          {
+            component: component,
+          }
+        );
+        console.log(chalk.green("✓") + ` Component ${component.name} synced`);
       } catch (e) {
         if (e.response.status === 422) {
-          await this.targetClient.put(`spaces/${this.targetSpaceId}/components/${this.getTargetComponentId(component.name)}`, {
-            component: component
-          })
-          console.log(chalk.green('✓') + ` Component ${component.name} synced`)
+          await this.targetClient.put(
+            `spaces/${
+              this.targetSpaceId
+            }/components/${this.getTargetComponentId(component.name)}`,
+            {
+              component: component,
+            }
+          );
+          console.log(chalk.green("✓") + ` Component ${component.name} synced`);
         } else {
-          console.error(chalk.red('X') + ` Component ${component.name} sync failed`)
+          console.error(
+            chalk.red("X") + ` Component ${component.name} sync failed`
+          );
         }
       }
     }
   },
 
-  getTargetComponentId (name) {
+  getTargetComponentId(name) {
     const comps = this.targetComponents.data.components.filter((comp) => {
-      return comp.name === name
-    })
+      return comp.name === name;
+    });
 
-    return comps[0].id
-  }
-}
+    return comps[0].id;
+  },
+};
 
 /**
  * @method sync
@@ -290,14 +373,14 @@ const SyncSpaces = {
  * @return {Promise}
  */
 const sync = (types, options) => {
-  SyncSpaces.init(options)
+  SyncSpaces.init(options);
 
-  const tasks = types.map(_type => {
-    const command = `sync${_type}`
-    return () => SyncSpaces[command]()
-  })
+  const tasks = types.map((_type) => {
+    const command = `sync${capitalizeFirstLetter(_type)}`;
+    return () => SyncSpaces[command]();
+  });
 
-  return pSeries(tasks)
-}
+  return pSeries(tasks);
+};
 
-module.exports = sync
+module.exports = sync;
